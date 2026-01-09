cdcab004b23ced6359a940460acd643f93439a73
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2026-01-09 03:05:48 -0500

    Remove aws.dev provider references from ecs-cluster.tf

--- terraform/ecs-cluster.tf
+++ terraform/ecs-cluster.tf
@@ -1,8 +1,6 @@
 data "aws_ami" "ecs" {
-  provider    = aws.dev
   most_recent = true
   owners      = ["self"]
-
   filter {
     name   = "name"
     values = [var.ecs_ami_name]
@@ -10,21 +8,21 @@ data "aws_ami" "ecs" {
 }
 
 resource "aws_ecs_cluster" "main" {
-  name     = "${var.project_name}-cluster"
-
+  name = "${var.project_name}-cluster"
+  
   setting {
     name  = "containerInsights"
     value = "enabled"
   }
-
+  
   tags = {
     Name = "${var.project_name}-cluster"
   }
 }
 
 resource "aws_iam_role" "ecs_instance" {
-  name     = "${var.project_name}-ecs-instance-role"
-
+  name = "${var.project_name}-ecs-instance-role"
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -40,34 +38,31 @@ resource "aws_iam_role" "ecs_instance" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_instance" {
-  provider   = aws.dev
   role       = aws_iam_role.ecs_instance.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ssm" {
-  provider   = aws.dev
   role       = aws_iam_role.ecs_instance.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ecs" {
-  name     = "${var.project_name}-ecs-instance-profile"
-  role     = aws_iam_role.ecs_instance.name
+  name = "${var.project_name}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance.name
 }
 
 resource "aws_launch_template" "ecs" {
-
   name_prefix   = "${var.project_name}-ecs-"
   image_id      = data.aws_ami.ecs.id
   instance_type = var.ecs_instance_type
-
+  
   iam_instance_profile {
     arn = aws_iam_instance_profile.ecs.arn
   }
-
+  
   vpc_security_group_ids = [aws_security_group.ecs.id]
-
+  
   user_data = base64encode(<<-EOF
               #!/bin/bash
               echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
@@ -75,11 +70,11 @@ resource "aws_launch_template" "ecs" {
               systemctl restart ecs
               EOF
   )
-
+  
   monitoring {
     enabled = true
   }
-
+  
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -89,24 +84,23 @@ resource "aws_launch_template" "ecs" {
 }
 
 resource "aws_autoscaling_group" "ecs" {
-
   name                = "${var.project_name}-ecs-asg"
   vpc_zone_identifier = [aws_subnet.private_app_1.id, aws_subnet.private_app_2.id]
   desired_capacity    = var.ecs_desired_capacity
   max_size            = var.ecs_max_size
   min_size            = var.ecs_min_size
-
+  
   launch_template {
     id      = aws_launch_template.ecs.id
     version = "$Latest"
   }
-
+  
   tag {
     key                 = "Name"
     value               = "${var.project_name}-ecs-instance"
     propagate_at_launch = true
   }
-
+  
   tag {
     key                 = "AmazonECSManaged"
     value               = true
