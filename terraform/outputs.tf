0b0590583927905ed577472d2ead86d12d75d312
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2026-01-12 18:31:47 -0500

    Fix outputs.tf syntax

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,15 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
 output "alb_dns_name" {
   value = aws_lb.main.dns_name
-}
-
 }
 
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.main.endpoint
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "website_url" {
-  description = "WordPress website URL"
+output "rds_endpoint" {
+  value = aws_db_instance.main.endpoint
 }
