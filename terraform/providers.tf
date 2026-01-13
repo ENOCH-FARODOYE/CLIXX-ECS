857c98d2124efe33ebc23b580b49fda4da124b94
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2026-01-12 20:37:26 -0500

    Add Route 53 cross-account DNS and HTTPS support

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,6 +12,15 @@ provider "aws" {
   region = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::529206289534:role/Engineer"
-    session_name = "terraform-clixx-ecs"
+    session_name = "terraform-dev"
+  }
+}
+
+provider "aws" {
+  alias  = "management"
+  region = var.aws_region
+  assume_role {
+    role_arn     = "arn:aws:iam::978820380225:role/Engineer"
+    session_name = "terraform-route53"
   }
 }
