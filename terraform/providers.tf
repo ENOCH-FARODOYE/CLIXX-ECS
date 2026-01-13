4925e0bf4e8409602f1b07559547aff3e7bb4e22
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2026-01-12 20:57:15 -0500

    Remove Route 53 cross-account dependency - deploy with ALB DNS only

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,12 +15,3 @@ provider "aws" {
     session_name = "terraform-dev"
   }
 }
-
-provider "aws" {
-  alias  = "management"
-  region = var.aws_region
-  assume_role {
-    role_arn     = "arn:aws:iam::978820380225:role/Engineer"
-    session_name = "terraform-route53"
-  }
-}
